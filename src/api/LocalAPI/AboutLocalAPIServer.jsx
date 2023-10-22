8b489c4acc3241b098a6d29eaf7232bf3f760cb7
RayanAnwer11 <mohanad.bme.ust@gmail.com>
2023-10-22 17:40:16 -0400

    back button

--- src/api/LocalAPI/AboutLocalAPIServer.jsx
+++ src/api/LocalAPI/AboutLocalAPIServer.jsx
@@ -27,11 +27,11 @@ const AboutLocalApiServer = () => {
     }
     return (
         <div>
+            <Link to="/" >
             <button className="rounded-md bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-900 text-white px-5 py-2 font-semibold transition duration-300 ease-in-out">
-                <Link to="/" >
                 Go Back
-                </Link>
                 </button>
+                </Link>
             <AboutLocalApiList bioList={bioData} />
         </div>
     )
