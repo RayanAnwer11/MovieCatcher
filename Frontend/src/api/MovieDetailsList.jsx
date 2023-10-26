b77d43c8d09d6d5f1f14f9718d1374f1e1ee9c5b
Kamron Anderson <Cruciangold1@gmail.com>
2023-10-26 14:48:57 -0400

    Genre detail

--- Frontend/src/api/MovieDetailsList.jsx
+++ Frontend/src/api/MovieDetailsList.jsx
@@ -1,28 +1,41 @@
-import MovieDetailsItem from "./MovieDetailsItem"
+import MovieDetailsItem from "./MovieDetailsItem";
 
-const MovieDetailsList = (props) =>{
-    return(
-        <ul>
-            {
-                
-                    <MovieDetailsItem
-                    key={props.detailed.id}
-                    title={props.detailed.title}
-                    overview={props.detailed.overview}
-                    voteAverage={props.detailed.vote_average}
-                    releaseDate={props.detailed.release_date}
-                    lang={props.detailed.original_language}
-                    imdb_id={props.detailed.imdb_id}
-                    id={props.detailed.id}
-                    homepage={props.detailed.homepage}
-                    image={props.detailed.poster_path}
-                    back_img={props.detailed.backdrop_path}
-                    />
-                
-                
-            }
-            
-        </ul>
-    )
-}
-export default MovieDetailsList
+const MovieDetailsList = (props) => {
+  if (
+    !props.detailed ||
+    !props.detailed.genres ||
+    !Array.isArray(props.detailed.genres)
+  ) {
+    return <div>No genre information available</div>;
+  }
+
+  const genreNames = [];
+  const genres = props.detailed.genres;
+
+  for (let i = 0; i < genres.length; i++) {
+    genreNames.push(genres[i].name);
+  }
+
+  const separatedGenreNames = genreNames.join(" ");
+  return (
+    <ul>
+      {
+        <MovieDetailsItem
+          key={props.detailed.id}
+          title={props.detailed.title}
+          overview={props.detailed.overview}
+          voteAverage={props.detailed.vote_average}
+          releaseDate={props.detailed.release_date}
+          lang={props.detailed.original_language}
+          imdb_id={props.detailed.imdb_id}
+          id={props.detailed.id}
+          homepage={props.detailed.homepage}
+          image={props.detailed.poster_path}
+          back_img={props.detailed.backdrop_path}
+          genre={separatedGenreNames}
+        />
+      }
+    </ul>
+  );
+};
+export default MovieDetailsList;
